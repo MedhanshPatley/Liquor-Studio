2154cd26c79a31f43811d9f52c06c27ef2e13137
MedhanshPatley <medhansh.patley.btech2021@sitpune.edu.in>
2024-04-09 14:38:18 +0530

    Starting of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import"./Navbar.css"
+import logo from '../Assets/Liquor Studio Logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="nav-logo">
+        <img src ={logo} alt =""/>
+      </div>
+        <ul className="nav-menu">
+          <li>Home <hr/></li>
+          <li>Category</li>
+          
+        </ul>
+        <div className="nav-login-cart">
+          <button>
+            Login
+          </button>
+          <img src ={cart_icon} alt =""/>
+          <div className="nav-cart-count">
+            0
+          </div>
+        </div>
     </div>
+      
+    
   )
 }
 
