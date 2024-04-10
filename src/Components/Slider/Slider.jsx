38d21f903bd448abde92cd4ed942eca0c9e0eb50
MedhanshPatley <medhansh.patley.btech2021@sitpune.edu.in>
2024-04-10 18:59:15 +0530

    Added auto transition

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,25 +1,24 @@
-import React, { useState } from 'react';
-import './Slider.css'; // You can create this CSS file for styling
-import image1 from '../Assets/Slider_img01.jpg'; // Import the first image
-import image2 from '../Assets/Slider_img02.jpg'; // Import the second image
+import React, { useState, useEffect } from 'react';
+import './Slider.css'; 
+import image1 from '../Assets/Slider_img01.jpg';
+import image2 from '../Assets/Slider_img02.jpg';
 
 const Slider = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const images = [image1, image2]; // Array of imported images
+  const images = [image1, image2]; 
 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  const prevImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-  };
+  useEffect(() => {
+    const interval = setInterval(nextImage, 1000); 
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="slider">
-      <button className="prev-button" onClick={prevImage}>{'<'}</button>
       <img className="slider-image" src={images[currentImageIndex]} alt={`Slide ${currentImageIndex + 1}`} />
-      <button className="next-button" onClick={nextImage}>{'>'}</button>
     </div>
   );
 };
