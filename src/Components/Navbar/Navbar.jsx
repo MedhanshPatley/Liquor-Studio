0acad44aa8194cddcf163909cde07c59e9bf1b2b
MedhanshPatley <medhansh.patley.btech2021@sitpune.edu.in>
2024-04-10 14:48:19 +0530

    Day2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 import"./Navbar.css"
 import logo from '../Assets/Liquor Studio Logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 const Navbar = () => {
+
+  const[menu,setmenu] = useState('Home');
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
         <img src ={logo} alt =""/>
       </div>
         <ul className="nav-menu">
-          <li>Home <hr/></li>
-          <li>Category</li>
+          <li onClick={()=>{setmenu('Home')}}>Home{menu==='Home'?<hr/>:<></>}</li>
+          <li onClick={()=>{setmenu('Whiskey')}}>Whiskey{menu==='Whiskey'?<hr/>:<></>}</li>
+          <li onClick={()=>{setmenu('Wine')}}>Wine{menu==='Wine'?<hr/>:<></>}</li>
+          <li onClick={()=>{setmenu('Vodka')}}>Vodka{menu==='Vodka'?<hr/>:<></>}</li>
           
         </ul>
         <div className="nav-login-cart">
