017fea1716786354fd5952dfad3fcf176e967303
MedhanshPatley <medhansh.patley.btech2021@sitpune.edu.in>
2024-04-15 23:33:39 +0530

    Added cart button to all product

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import './Item.css'
 
+
 const items = (props) => {
   return (
     <div className='item'>
       <img src={props.image}/>
-      <p>
+      <p className='item_name'>
         {props.name}
       </p>
       <div className="item_prices">
         <div className="item_price_new">
-        Rs. {props.new_price}/-
+        Rs.{props.new_price}/-
         </div>
         <div className="item_price_old">
-        Rs. {props.old_price}/-
-
+        Rs.{props.old_price}/- 
         </div>
       </div>
+      <button className='Add_to_cart'>Add to cart</button>
       
     </div>
   )
